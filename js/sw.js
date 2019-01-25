3b7ba0c327d91920eb213fe657e2378b1c6d7cdd
Mariam <mariam.fouad94@gmail.com>
2019-01-25 22:28:14 +0300

    add event listener for the activate

--- js/sw.js
+++ js/sw.js
@@ -1,29 +1,28 @@
-let restaurantsCacheName = 'restaurants-v1';
+let restaurantsCacheName = 'restaurants-reviews-v3';
 
 self.addEventListener('install', event=>{
     event.waitUntil(
-        aches.open(staticCacheName)
+        caches.open(restaurantsCacheName )
             .then(cache=>{
                 return cache.addAll([
-                    './',
-                    './index.html',
-                    './restaurant.html',
-                    './css/styles.css',
-                    './data/restaurants.json',
-                    './js/dbhelper.js',
-                    './js/main.js',
-                    './js/restaurant_info.js',
-                    './js/sw_register.js',
-                    './img/1.jpg',
-                    './img/2.jpg',
-                    './img/3.jpg',
-                    './img/4.jpg',
-                    './img/5.jpg',
-                    './img/6.jpg',
-                    './img/7.jpg',
-                    './img/8.jpg',
-                    './img/9.jpg',
-                    './img/10.jpg'
+                    '/',
+                    '/index.html',
+                    '/restaurant.html',
+                    '/css/styles.css',
+                    '/data/restaurants.json',
+                    '/img/1.jpg',
+                    '/img/2.jpg',
+                    '/img/3.jpg',
+                    '/img/4.jpg',
+                    '/img/5.jpg',
+                    '/img/6.jpg',
+                    '/img/7.jpg',
+                    '/img/8.jpg',
+                    '/img/9.jpg',
+                    '/img/10.jpg',
+                    '/js/main.js',
+                    '/js/dbhelper.js',
+                    '/js/restaurant_info.js'
                 ]);
             })
     )
@@ -40,4 +39,20 @@ self.addEventListener('fetch', event=> {
                 return fetch(event.request);
             })
     )
+});
+
+self.addEventListener('activate', event=> {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames=>{
+                return Promise.all(
+                    cacheNames.filter(cacheName=> {
+                        return cacheName.startsWith('restaurants-reviews-v') && cacheName != restaurantsCacheName;
+                    }).map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+    )
+
 });
