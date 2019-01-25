1d98bec8456de6b3977365b1c3d81a898efc966d
Mariam <mariam.fouad94@gmail.com>
2019-01-25 20:28:27 +0300

    cache the resourses

---
+++ js/sw.js
@@ -0,0 +1,31 @@
+let restaurantsCacheName = 'restaurants-v1';
+
+self.addEventListener('install', event=>{
+    event.waitUntil(
+        aches.open(staticCacheName)
+            .then(cache=>{
+                return cache.addAll([
+                    './',
+                    './index.html',
+                    './restaurant.html',
+                    './css/styles.css',
+                    './data/restaurants.json',
+                    './js/dbhelper.js',
+                    './js/main.js',
+                    './js/restaurant_info.js',
+                    './js/sw_register.js',
+                    './img/1.jpg',
+                    './img/2.jpg',
+                    './img/3.jpg',
+                    './img/4.jpg',
+                    './img/5.jpg',
+                    './img/6.jpg',
+                    './img/7.jpg',
+                    './img/8.jpg',
+                    './img/9.jpg',
+                    './img/10.jpg'
+                ]);
+            })
+    )
+
+});
